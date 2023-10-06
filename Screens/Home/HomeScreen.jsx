6af1a9b400be2ca358801515f1b3c303bf09f57b
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-10-06 17:24:28 +0700

    HomeScreen

--- Screens/Home/HomeScreen.jsx
+++ Screens/Home/HomeScreen.jsx
@@ -19,7 +19,44 @@ const HomeScreen = () =>{
       nameProduct: 'Good Food', 
       min:'14 min' 
     },
+    { id: 4,
+      image: require('../../assets/Home/mage.png'), 
+      nameProduct: 'Vegan Resto', 
+      min:'12 min' 
+    },
+    { id: 5,
+      image: require('../../assets/Home/Heaththy.png'), 
+      nameProduct: 'Vegan Resto', 
+      nameProduct: 'Healthy Resto', 
+      min:'13 min' 
+    },
+    { id: 6,
+      image: require('../../assets/Home/luamach.png'), 
+      nameProduct: 'Vegan Resto', 
+      nameProduct: 'Good Food', 
+      min:'14 min' 
+    },
   ];
+  const Menu =[
+    { id: 1,
+      image: require('../../assets/Home/MenuPhoto.png'), 
+      nameMenu: 'Herbar Pancake',
+      name:'Warung Herbal',
+      price:'$7' 
+    },
+    { id: 2,
+      image: require('../../assets/Home/Menu.png'), 
+      nameMenu: 'Fruit Salad', 
+      name:'Wijie Resto',
+      price:'$5' 
+    },
+    { id: 3,
+      image: require('../../assets/Home/Photo.png'), 
+      nameMenu: 'Green  Noddle',
+      name:'Noodle Home',
+      price:'$15' 
+    },
+  ]
     return(
         <ImageBackground source={require('../../assets/Home/Homebackground.png')}style={styles.imageBackground}>
             <ScrollView>
@@ -67,6 +104,30 @@ const HomeScreen = () =>{
                           </View>
                           <Text style={styles.textVegan}>{item.nameProduct}</Text>
                           <Text style={styles.textMin}>{item.min}</Text>
+                        </View>
+                      )}
+                />
+               <View style={styles.viewNeVi}>
+                    <Text style={styles.textNe}>Popular Menu</Text>
+                    <Text style={styles.textviewMore}>View More</Text>
+                </View>
+                <FlatList
+                      data={Menu}
+                      horizontal={false}
+                      style={styles.viewListItem}
+                      contentContainerStyle={{gap: 31, width:'100%' }}
+                      renderItem={({ item }) => (
+                        <View style={styles.viewMenu} key={item.id}>
+                          <View style={styles.viewImageMenu}>
+                            <Image source={item.image} />
+                            <View style={styles.viewTexts}>
+                              <Text style={styles.textmenu}>{item.nameMenu}</Text>
+                              <Text style={styles.textName}>{item.name}</Text>
+                            </View>
+                          </View>
+                          <View style={styles.viewPrice}>
+                              <Text style={styles.textPrice}>{item. price}</Text>
+                          </View>
                         </View>
                       )}
                 />
@@ -190,7 +251,6 @@ const styles = StyleSheet.create({
         shadowRadius: 50,
         elevation: 5, // Điều chỉnh giá trị để thay đổi độ sâu của bóng đổ (cho Android)
       }
-    
     ,viewImage:{
         paddingLeft:21,
         paddingRight:30,
@@ -214,8 +274,54 @@ const styles = StyleSheet.create({
         lineHeight:17.03,
         color:'#22242E',
         textAlign:'center',
-    }
+    } ,
+    viewMenu:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      backgroundColor: 'white',
+      borderWidth:1,
+      borderColor:'#6B50F6',
+      borderRadius: 22,
+      shadowColor: 'rgba(90, 108, 234, 0.07)',
+      shadowOffset: { width: 12, height: 26 },
+      shadowOpacity: 1,
+      shadowRadius: 50,
+      elevation: 5, // Điều chỉnh giá trị để thay đổi độ sâu của bóng đổ (cho Android)
+      paddingLeft:10,
+      paddingTop:12,
+      paddingBottom:12,
+      paddingRight:29
+    } ,
+    viewImageMenu:{
+        flexDirection:'row',
+        gap:21,
+        
+    } ,
+    viewTexts:{
+      paddingTop:13
+    },
+    textmenu:{
+      fontSize:15,
+      fontWeight:'500',
+      lineHeight:19.46,
+      color:'#22242E'
+    },
+    textName:{
+      fontSize:13,
+      color:'gray',
+      fontWeight:'400',
+    },
+    viewPrice:{
+      paddingTop:18,
+      paddingBottom:20,
       
+    },
+    textPrice:{
+      fontSize:22,
+      fontWeight:'600',
+      color:'#6B50F6',
+      lineHeight:28.83
+    }
 }
   
 )  
