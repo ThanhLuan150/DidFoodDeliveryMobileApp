da713322c84da048b741b4b46313238a3ec015bb
vannhat0511 <116283925+vannhat0511@users.noreply.github.com>
2023-10-23 12:41:05 +0700

    ProfileScreen

--- Screens/Profile/ProfileScreen.jsx
+++ Screens/Profile/ProfileScreen.jsx
@@ -1,127 +1,241 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-} from "react-native";
-
-const ProfileScreen = ({ navigation }) => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  const handleScroll = () => {
-    setIsScrolled(!isScrolled);
-  };
-
+import {StyleSheet,View,Text,ScrollView,Image,TouchableOpacity,} from "react-native";
+const ProfileScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Profile</Text>
-      </View>
-
-      <View style={styles.content}>
-        <View style={styles.profile}>
-          <Image
-            source={require("../../assets/Profile/PhotoProfile.png")}
-            style={styles.profileImage}
-          />
-          <Text style={styles.profileName}>Arash Ranjbaran</Text>
-          <Text style={styles.profileEmail}>awdesign.ar@gmail.com</Text>
-          <Text style={styles.profileVoucher}>
-            You Have 3 Voucher
-          </Text>
-        </View>
-
-        <ScrollView
-          onScroll={handleScroll}
-          scrollEnabled={isScrolled}
-          showsVerticalScrollIndicator={false}
-        >
-          <View style={styles.favorites}>
-            <TouchableOpacity
-              style={styles.favorite}
-              onPress={() => {
-                navigation.navigate("Favorite");
-              }}
-            >
-              <Image
-                source={require("../../assets/Profile/PhotoProfile.png")}
-                style={styles.favoriteImage}
-              />
-              <Text style={styles.favoriteName}>Spacy fresh crab</Text>
-              <Text style={styles.favoritePrice}>
-                $35
-              </Text>
+      <Image style={styles.Image}  source={require('../../assets/Profile/PhotoProfile.png')}></Image>
+      <ScrollView style={styles.ScrollView}>
+          <View style={styles.viewScrollView}>
+            <TouchableOpacity style={styles.ScrollTool}></TouchableOpacity>
+          </View>
+          <View style={styles.viewButtonMember}>
+            <TouchableOpacity style={styles.ButtonMember}>
+              <Text style={styles.textMember}>Member Gold</Text>
             </TouchableOpacity>
           </View>
-        </ScrollView>
-      </View>
+          <View style={styles.viewNameProfile}>
+            <Text style={styles.textNameProfile}>Arash Ranjbaran</Text>
+            <TouchableOpacity style={styles.TouchableOpacity}>
+              <Image source={require('../../assets/Profile/EditIcon.png')}></Image>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.textEmail}>awdesign.ar@gmail.com</Text>
+          <View style={styles.viewVoucher}>
+            <View style={styles.viewVoucherItem}>
+              <Image source={require('../../assets/Profile/VoucherIcon.png')}></Image>
+              <Text style={styles.textVoucher}>You have 3 Voucher</Text>
+            </View>
+          </View>
+          <View style={styles.viewFavorite}>
+            <Text style={styles.textFacevorite}>Favorite</Text>
+          </View>
+          <View style={styles.viewListFavorite}>
+            <View style={styles.viewListItemFavorite}>
+                <Image source={require('../../assets/Profile/PhotoMenu1.png')}></Image>
+                <View style={styles.viewContent}>
+                  <Text style={styles.textFavorite}>Spacy fresh crab</Text>
+                  <Text style={styles.textWaroenk}>Waroenk kita</Text>
+                  <Text style={styles.textPrice}>$ 35</Text>
+                </View>
+                <View style={styles.viewButtonBuyAgain}>
+                  <TouchableOpacity style={styles.ButtonBuyAgain}>
+                    <Text style={styles.textBuyAgain}>Buy Again</Text>
+                  </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.viewListItemFavorite}>
+                <Image source={require('../../assets/Profile/PhotoMenu2.png')}></Image>
+                <View style={styles.viewContent}>
+                  <Text style={styles.textFavorite}>Spacy fresh crab</Text>
+                  <Text style={styles.textWaroenk}>Waroenk kita</Text>
+                  <Text style={styles.textPrice}>$ 35</Text>
+                </View>
+                <View style={styles.viewButtonBuyAgain}>
+                  <TouchableOpacity style={styles.ButtonBuyAgain}>
+                    <Text style={styles.textBuyAgain}>Buy Again</Text>
+                  </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.viewListItemFavorite}>
+                <Image source={require('../../assets/Profile/PhotoMenu.png')}></Image>
+                <View style={styles.viewContent}>
+                  <Text style={styles.textFavorite}>Spacy fresh crab</Text>
+                  <Text style={styles.textWaroenk}>Waroenk kita</Text>
+                  <Text style={styles.textPrice}>$ 35</Text>
+                </View>
+                <View style={styles.viewButtonBuyAgain}>
+                  <TouchableOpacity style={styles.ButtonBuyAgain}>
+                    <Text style={styles.textBuyAgain}>Buy Again</Text>
+                  </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.viewListItemFavorite}>
+                <Image source={require('../../assets/Profile/PhotoMenu.png')}></Image>
+                <View style={styles.viewContent}>
+                  <Text style={styles.textFavorite}>Spacy fresh crab</Text>
+                  <Text style={styles.textWaroenk}>Waroenk kita</Text>
+                  <Text style={styles.textPrice}>$ 35</Text>
+                </View>
+                <View style={styles.viewButtonBuyAgain}>
+                  <TouchableOpacity style={styles.ButtonBuyAgain}>
+                    <Text style={styles.textBuyAgain}>Buy Again</Text>
+                  </TouchableOpacity>
+                </View>
+            </View>
+          </View>
+      </ScrollView>
     </View>
   );
-};
-
+};  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
+  container:{
+    flex:1,
   },
-  header: {
-    height: 50,
-    backgroundColor: "#000000",
-    alignItems: "center",
+  Image:{
+    width:'100%'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#ffffff",
+  ScrollView:{
+    backgroundColor:'white',
+    borderTopStartRadius:30,
   },
-  content: {
-    flex: 1,
-    padding: 20,
+  viewScrollView:{
+    paddingLeft:160,
+    paddingRight:160,
+    paddingTop:18
   },
-  profile: {
-    flex: 1,
-    alignItems: "center",
+  ScrollTool:{
+    backgroundColor:'#FEF6ED',
+    width:'100%',
+    height:5,
+    borderRadius:12
   },
-  profileImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+  viewButtonMember:{
+    paddingLeft:20,
+    paddingRight:257,
+    paddingTop:28
   },
-  profileName: {
-    fontSize: 16,
-    fontWeight: "bold",
+  ButtonMember:{
+    backgroundColor:'rgba(0, 255, 102, 0.10)',
+    paddingLeft:17,
+    paddingRight:17,
+    paddingTop:10,
+    paddingBottom:10,
+    borderRadius:18
   },
-  profileEmail: {
-    fontSize: 14,
+  textMember:{
+    color:'#6B50F6',
+    fontSize:12,
+    fontWeight:'500'
   },
-  profileVoucher: {
-    fontSize: 14,
-    color: "#000000",
+  viewNameProfile:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    paddingTop:20,
+    paddingLeft:21,
+    paddingRight:32
   },
-  favorites: {
-    flex: 1,
-    padding: 20,
+  textNameProfile:{
+    color:'#22242E',
+    fontSize:27,
+    fontWeight:"600",
   },
-  favorite: {
-    flexDirection: "row",
-    alignItems: "center",
+  TouchableOpacity:{
+    marginTop:10
   },
-  favoriteImage: {
-    width: 50,
-    height: 50,
+  textEmail:{
+    color:'gray',
+    paddingLeft:21,
+    paddingTop:4
   },
-  favoriteName: {
-    fontSize: 16,
-    marginLeft: 10,
+  viewVoucher:{
+    paddingTop:20,
+    paddingLeft:15,
+    paddingRight:15
   },
-  favoritePrice: {
-    fontSize: 14,
-    color: "#000000",
-    marginLeft: 10,
+  viewVoucherItem:{
+    flexDirection:'row',
+    gap:16,
+    backgroundColor:'white',
+    elevation: 2, 
+    shadowOpacity: 1,
+    shadowRadius: 5 ,
+    paddingBottom:8,
+    borderRadius:22,
+    paddingTop:16,
+    paddingLeft:16
   },
+  textVoucher:{
+    marginTop:7,
+    marginBottom:7,
+    color:'#22242E',
+    fontSize:14,
+    fontWeight:'500'
+  },
+  viewFavorite:{
+    paddingTop:20,
+    paddingLeft:34,
+    paddingRight:283
+  },
+  textFacevorite:{
+    color:'#22242E',
+    fontSize:15,
+    fontWeight:'600'
+  },
+  viewListFavorite:{
+    paddingTop:20,
+    paddingLeft:15,
+    paddingRight:15,
+    flexDirection:'column',
+    gap:20
+  },
+  viewListItemFavorite:{
+    flexDirection:'row',
+    paddingLeft:14,
+    paddingRight:17,
+    paddingTop:21,
+    paddingBottom:21,
+    borderRadius:22,
+    backgroundColor:'white',
+    elevation: 2, 
+    shadowOpacity: 1,
+    shadowRadius: 5 ,
+  },
+  viewContent:{
+    paddingLeft:17.45
+  },
+  textFavorite:{
+    color:'#22242E',
+    fontSize:15,
+    fontWeight:'600',
+    lineHeight:19.65
+  },
+  textWaroenk:{
+    color:'gray',
+    fontSize:14
+  },
+  textPrice:{
+    color:'#6B50F6',
+    fontSize:19,
+    fontWeight:'600',
+    marginTop:4
+  },
+  viewButtonBuyAgain:{
+    marginTop:17,
+    paddingLeft:32
+  },
+  ButtonBuyAgain:{
+    backgroundColor:'#6B50F6',
+    paddingTop:9,
+    paddingBottom:9,
+    paddingLeft:12,
+    paddingRight:12,
+    borderRadius:17.50
+  },
+  textBuyAgain:{
+    color:'white',
+    fontSize:12,
+    fontWeight:'500',
+  }
 });
 
 export default ProfileScreen;
