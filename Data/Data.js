93a7ebd09f37eb8a57212434c9276be3a744cb5c
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-10-26 13:33:55 +0700

    UpdateDataInterface

---
+++ Data/Data.js
@@ -0,0 +1,76 @@
+
+  const fakeData = {
+    products: [
+      {
+        id: 1,
+        image:"../assets/Home/mage.png",
+        nameProduct: "Vegan Resto",
+        min:'12 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+      },
+      {
+        id: 2,
+        nameProduct: "Healthy Resto",
+        min:'13 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . ."
+      },
+      {
+        id: 3,
+        nameProduct: "Good Food",
+        min:'14 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+     
+      },
+      {
+        id: 4,
+        nameProduct: "Vegan Resto",
+        min:'12 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+        
+      },
+      {
+        id: 5,
+        nameProduct: "Healthy Resto",
+        min:'13 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+       
+      },
+      {
+        id: 6,
+        nameProduct: "Healthy Resto",
+        min:'13 min',
+        description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+       
+      },
+    ],
+    menu: [
+      {
+        id: 1,
+        nameMenu: "Herbar Pancake",
+        name:'Wijie Resto',
+        image: "../assets/Home/MenuPhoto.png",
+        description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+        price: "$15",
+      },
+      {
+        id: 2,
+        nameMenu: "Fruit Salad",
+        name:'Wijie Resto',
+        image: "../assets/Home/Menu.png",
+        description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+        price: "$20",
+      },
+      {
+        id: 3,
+        nameMenu: "Green Noddle",
+        name:'Noodle Home',
+        image: "../assets/Home/Photo.png",
+        description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+        price: "$25",
+      },
+    ],
+  };
+  
+  export default fakeData;
+  
+  
